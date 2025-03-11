60958cf6d446f016164c462d51aa4306d0387ce7
Aramex86 <slavbez@mail.ru>
2025-03-11 18:03:21 +0200

    testAi1

--- src/Entities/service/openai.ts
+++ src/Entities/service/openai.ts
@@ -1,21 +1,18 @@
 export default async function getOpenAIResponse(prompt: string) {
   console.log(import.meta.env.VITE_OPENAI_API_KEY, "key");
   console.log(prompt, "prompt");
-  const response = await fetch(
-    "https://coverletter-ai.free.beeceptor.com/todos",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
-      },
-      body: JSON.stringify({
-        model: "gpt-4o-mini",
-        prompt,
-        maxTokens: 150,
-      }),
-    }
-  );
+  const response = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-4o-mini",
+      prompt,
+      maxTokens: 150,
+    }),
+  });
 
   const { choices } = await response.json();
   return choices[0].text.trim();
